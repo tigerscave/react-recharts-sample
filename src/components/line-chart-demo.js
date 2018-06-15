0da06a09a0b8870469d81b76ef1335046145ea7b
rhmtrz <2249301106@qq.com>
2018-06-15 22:55:52 +0900

    chart: line chart

--- src/components/line-chart-demo.js
+++ src/components/line-chart-demo.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import {
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend 
+} from 'recharts';
 
 export default class LineChartDemo extends React.Component {
   render() {
     const { data } = this.props;
     console.log(data);
     return (
-      <p>{typeof data}</p>
+      <LineChart width={600} height={300} data={data}
+            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+        <XAxis dataKey="period"/>
+        <YAxis/>
+        <CartesianGrid strokeDasharray="3 3"/>
+        <Tooltip/>
+        <Legend />
+        <Line type="line" dataKey="totalSales" stroke="red" activeDot={{r: 8}}/>
+      </LineChart>
     );
   }
 }
+
+
+
+
