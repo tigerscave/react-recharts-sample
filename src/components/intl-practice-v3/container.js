dd931db1f58ca14f1e9469ce24daee71cddb7d12
rhmtrz <2249301106@qq.com>
2018-07-02 14:54:25 +0900

    feat(intl-practice-v3): change language by browser default language

--- src/components/intl-practice-v3/container.js
+++ src/components/intl-practice-v3/container.js
@@ -1,15 +1,51 @@
 import React from 'react';
+import { IntlProvider, addLocaleData } from 'react-intl'
+
+import en from 'react-intl/locale-data/en'; 
+import ja from 'react-intl/locale-data/ja'; 
+import zh from 'react-intl/locale-data/zh'; 
+
+import { en_US, ja_JP, zh_CN } from '../../languages2'; 
+addLocaleData([...en, ...ja, ...zh]); 
+
+import IntroductionContent from './introduction-content'; 
+
+const languageDictionaries = {
+  en: en_US, 
+  ja: ja_JP,
+  zh: zh_CN,
+};
 
 export default class IntlPracticeContainer extends React.Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      language: 'en',
+    }
+  }
+  componentDidMount = () => {
+    console.log("hello did mount")
+    this.setState({
+      language: window.navigator.languages[0],
+    })
+    console.warn(window.navigator.languages);
+  }
 
   render() {
+    const { language } = this.state;
+    const messages =
+      languageDictionaries[language]
+
     return (
-      <div>
-        <h1>Change browser language setting</h1>
-        <p>My name is Rahmat.</p>
-        <p>I am from Kabul</p>
-        <p>I like to eat Biryiani</p>
-      </div>
+      <IntlProvider
+        key={language}
+        locale={language}
+        messages={messages} 
+      >
+        <div>
+          <IntroductionContent />
+        </div>
+      </IntlProvider>
     );
   }
 }
